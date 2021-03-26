853784d0af70ff910976d4a18b8dee28f0cd4399
Your Name <you@example.com>
2021-03-25 19:27:06 -0600

    Cambios con los formularios y el componente de heroes 2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 
+import {HEROES} from '../mock-heroes';
+
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -8,16 +11,15 @@ import {Hero} from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero ={
-    id: 1,
-    name: 'Spiderman',
-    edad: 23, 
-    poder: 'Telaraña',
-    identidad: 'Peter Parker',
-    universo: 'Marvel'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
+
   ngOnInit(): void {
+    
   }
+    onSelect(hero: Hero): void {
+      this.selectedHero = hero;
+    }
 }
