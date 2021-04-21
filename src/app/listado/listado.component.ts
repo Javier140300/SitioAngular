23c259a143820a548786050475eab1540b07976c
Your Name <you@example.com>
2021-04-20 23:41:31 -0500

    Practica Boton Eliminar Heroe

---
+++ src/app/listado/listado.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent  {
+  
+  heroes: string [] = [ 'Iroman', 'Doctor Strange', 'Black Panter',  'Jhon Wick', 'Thanos'];
+  messages: string [] = [];
+
+  add(messages: string){
+    this.messages.push(messages);
+  }
+
+  Borrar(){
+    var heroe= this.messages.push(this.heroes[0]);
+    this.heroes.shift();
+    return heroe;
+  }
+
+}
